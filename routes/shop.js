91c9677437620b4049ec62506ac4176930d55c6a
Harit012 <rabadiyaharit@gmail.com>
2024-02-20 10:21:53 +0530

    Tamplating Engines:- pug file

--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,20 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
+const adminData = require("./admin");
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+router.get("/", (req, res, next) => {
+  console.log(adminData.products);
+  // res.sendFile(path.join(rootDir, "views", "shop.html"));
+  res.render("shop", {
+    prods: adminData.products,
+    pageTitle: "My Shop",
+    path: "/",
+  });
 });
 
 module.exports = router;
