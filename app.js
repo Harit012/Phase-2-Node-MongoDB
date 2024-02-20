f478b720633aad2c4bb2a35c026b48788c08d569
Harit012 <rabadiyaharit@gmail.com>
2024-02-20 12:06:15 +0530

    Templating Engines:- Express-Handlebars

--- app.js
+++ app.js
@@ -1,19 +1,58 @@
-const express = require("express");
-const path = require("path");
-const body_parser = require("body-parser");
-const adminData = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+// const express = require("express");
+// const path = require("path");
+// const body_parser = require("body-parser");
+// const expressHbs = require("express-handlebars");
+// // import { engine } from 'express-handlebars';
+
+// const app = express();
+
+// app.engine("hbs", expressHbs());
+// app.set("view engine", "handlebarss");
+// // app.set("view engine", "pug");
+// app.set("views", "views");
+
+// const adminData = require("./routes/admin");
+// const shopRoutes = require("./routes/shop");
+
+// app.use(body_parser.urlencoded({ extended: false }));
+// app.use(express.static(path.join(__dirname, "public")));
+
+// app.use("/admin", adminData.routes);
+// app.use(shopRoutes);
+// app.use((req, res, next) => {
+//   res.status(404).render("404", { pageTitle: "Page Not Found" });
+// });
+// app.listen(3000);
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const app = express();
-app.set("view engine", "pug");
-app.set("views", "views");
 
-app.use(body_parser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.engine(
+  'hbs',
+  expressHbs({
+    layoutsDir: 'views/layouts/',
+    defaultLayout: 'main-layout',
+    extname: 'hbs'
+  })
+);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
-app.use("/admin", adminData.routes);
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
+
 app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found" });
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
+
 app.listen(3000);
