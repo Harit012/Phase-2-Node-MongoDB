7babf68cbd62f490d76958ce032ebffd928d54d7
Harit012 <rabadiyaharit@gmail.com>
2024-02-21 17:39:23 +0530

    Module 9 base code

--- models/product.js
+++ models/product.js
@@ -19,8 +19,11 @@ const getProductsFromFile = (cb) => {
 const products = [];
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title,imageUrl,price,description) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.price = price;
+    this.description = description;
   }
 
   save() {
